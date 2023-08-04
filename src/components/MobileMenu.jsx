899268f50e553f1a5b1c5ab6c35933c720ab81ff
Grygory Ksheminsky <maroon785@gmail.com>
2023-08-04 17:01:05 +0300

    Finished MobileMenu and working with state

---
+++ src/components/MobileMenu.jsx
@@ -0,0 +1,55 @@
+import { AiFillTag, AiOutlineClose } from 'react-icons/ai';
+import { BsFillSaveFill } from 'react-icons/bs';
+import { TbTruckDelivery } from 'react-icons/tb';
+import { FaUserFriends, FaWallet } from 'react-icons/fa';
+import { MdFavorite, MdHelp } from 'react-icons/md';
+import { useState } from 'react';
+
+function MobileMenu({ onCloseMobile }) {
+  return (
+    <>
+      <div
+        onClick={() => onCloseMobile()}
+        className="bg-black/50 fixed w-full h-screen z-10 top-0 left-0"></div>
+
+      <div className="fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300 text-2xl">
+        <AiOutlineClose
+          onClick={() => onCloseMobile()}
+          size={20}
+          className="absolute top-3 right-3 cursor-pointer"
+        />
+        <h2 className="p-5">
+          Укр <span className="text-orange-500 font-bold ">БУРГЕР</span>
+        </h2>
+        <nav>
+          <ul className="flex flex-col p-4 text-grey-800">
+            <li className="text-xl py-4 flex items-center">
+              <TbTruckDelivery size={25} className="mr-4" /> Закази
+            </li>
+            <li className="text-xl py-4 flex items-center">
+              <MdFavorite size={25} className="mr-4" />
+              Улюблене
+            </li>
+            <li className="text-xl py-4 flex items-center">
+              <FaWallet size={25} className="mr-4" /> Гаманець
+            </li>
+            <li className="text-xl py-4 flex items-center">
+              <MdHelp size={25} className="mr-4" /> Помiч
+            </li>
+            <li className="text-xl py-4 flex items-center">
+              <AiFillTag size={25} className="mr-4" /> Робота
+            </li>
+            <li className="text-xl py-4 flex items-center">
+              <BsFillSaveFill size={25} className="mr-4" /> Найкраще
+            </li>
+            <li className="text-xl py-4 flex items-center">
+              <FaUserFriends size={25} className="mr-4" /> Запроси друга
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default MobileMenu;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,21 @@
 import { AiOutlineMenu } from 'react-icons/ai';
 import { BsFillCartFill } from 'react-icons/bs';
 import Search from './Search';
+import MobileMenu from './MobileMenu';
+import { useState } from 'react';
 
 function Navbar() {
+  const [nav, setNav] = useState(false);
+
+  const closeMobileMenu = () => {
+    setNav(false);
+  };
+
   return (
     <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
       <div className="flex items-center">
-        <div className="cursor-pointer">
-          <AiOutlineMenu size={30} />
+        <div className="cursor-pointer mr-3">
+          <AiOutlineMenu size={30} onClick={() => setNav(!nav)} />
         </div>
         <h1 className="text-2xl sm:text-3xl md:text-4xl">
           Укр <span className="uppercase text-orange-500 font-bold">бургер</span>
@@ -18,6 +26,8 @@ function Navbar() {
       <button className="bg-black text-white flex flex-col items-center justify-center">
         <BsFillCartFill className="" /> Cart
       </button>
+
+      {nav && <MobileMenu onCloseMobile={closeMobileMenu} />}
     </div>
   );
 }

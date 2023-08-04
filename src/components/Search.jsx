f03c774519be29c10d13513c4b124fc53c5c0e49
Grygory Ksheminsky <maroon785@gmail.com>
2023-08-04 16:00:12 +0300

    Finished working on Search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,11 @@ function Search() {
   return (
     <div className="flex items-center bg-gray-200 rounded-full px-2 w-[200px] sm:w-[400px] lg-[500px]">
       <AiOutlineSearch size={20} />
-      <input type="text" placeholder="Шукати їжу..." />
+      <input
+        className="bg-transparent ml-5 p-2 focus:outline-none w-full"
+        type="text"
+        placeholder="Шукати їжу..."
+      />
     </div>
   );
 }

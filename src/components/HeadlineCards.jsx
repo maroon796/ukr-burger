8c91ab4f5b7335164b022f45f3943b402fc5d130
Grygory Ksheminsky <maroon785@gmail.com>
2023-08-06 21:57:31 +0300

    Finished MainFood compomemt

---
+++ src/components/HeadlineCards.jsx
@@ -0,0 +1,5 @@
+function HeadlineCards() {
+  return <div></div>;
+}
+
+export default HeadlineCards;
--- src/components/MainFood.jsx
+++ src/components/MainFood.jsx
@@ -2,15 +2,16 @@ function MainFood() {
   return (
     <div className="max-w-[1640px] mx-auto p-4">
       <div className="max-h-[500px] relative">
-        <div  className="absolute">
-          <h1>
-            Кращi <span>бургери</span>
+        <div className="absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center">
+          <h1 className="p-4 text-4xl sm:text-5xl font-bold">
+            Кращi <span className="text-orange-500">бургери</span>
           </h1>
-          <h1>
-            Залюбки <span>доставимо</span>
+          <h1 className="p-4 text-4xl sm:text-5xl font-bold">
+            Залюбки <span className="text-orange-500">доставимо</span>
           </h1>
         </div>
         <img
+          className="w-full max-h-[500px] object-cover"
           src="https://images.pexels.com/photos/13163534/pexels-photo-13163534.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="/"
         />

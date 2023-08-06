5dca989b09466cd26ec5fe87e990912f9872805a
Grygory Ksheminsky <maroon785@gmail.com>
2023-08-06 22:36:51 +0300

    Created data dile with meals

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Food from './components/Food';
 import HeadlineCards from './components/HeadlineCards';
 import MainFood from './components/MainFood';
 import Navbar from './components/Navbar';
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <MainFood />
       <HeadlineCards />
+      <Food />
     </div>
   );
 }
---
+++ src/data/data.js
@@ -0,0 +1,98 @@
+export const data = [
+  {
+    id: 1,
+    name: 'Double Cheese',
+    category: 'burger',
+    price: '$$$$',
+    image:
+      'https://images.pexels.com/photos/5554607/pexels-photo-5554607.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 2,
+    name: 'Bacon Cheese',
+    category: 'burger',
+    price: '$',
+    image:
+      'https://images.pexels.com/photos/3738762/pexels-photo-3738762.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 3,
+    name: 'Mushroom Burger',
+    category: 'burger',
+    price: '$$',
+    image:
+      'https://images.pexels.com/photos/5908059/pexels-photo-5908059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 4,
+    name: 'Loaded Burger',
+    category: 'burger',
+    price: '$$$',
+    image:
+      'https://images.pexels.com/photos/3616956/pexels-photo-3616956.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 5,
+    name: 'Feta Pizza',
+    category: 'pizza',
+    price: '$$$$',
+    image:
+      'https://images.pexels.com/photos/14866675/pexels-photo-14866675.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 6,
+    name: 'Vegan Pizza',
+    category: 'pizza',
+    price: '$$$',
+    image:
+      'https://images.pexels.com/photos/7172067/pexels-photo-7172067.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 7,
+    name: 'Meat Pizza',
+    category: 'pizza',
+    price: '$$',
+    image:
+      'https://images.pexels.com/photos/842519/pexels-photo-842519.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 8,
+    name: 'Supreme Pizza',
+    category: 'pizza',
+    price: '$',
+    image:
+      'https://images.pexels.com/photos/4773769/pexels-photo-4773769.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 9,
+    name: 'Meat Salad',
+    category: 'salad',
+    price: '$$$$',
+    image:
+      'https://images.pexels.com/photos/551997/pexels-photo-551997.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 10,
+    name: 'Light Salad',
+    category: 'salad',
+    price: '$$$',
+    image:
+      'https://images.pexels.com/photos/257816/pexels-photo-257816.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 11,
+    name: 'Italian Salad',
+    category: 'salad',
+    price: '$$',
+    image:
+      'https://images.pexels.com/photos/806361/pexels-photo-806361.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 12,
+    name: 'Picanto Salad',
+    category: 'salad',
+    price: '$',
+    image:
+      'https://images.pexels.com/photos/1143754/pexels-photo-1143754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+];
